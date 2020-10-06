7961b819a91de5f27755e0d2ef3a566235c0ec90
carlos <carloscarias>
2020-10-06 02:51:16 +0000

    servidor a medias

--- API/app.py
+++ API/app.py
@@ -1,37 +1,58 @@
 import os
-from flask import Flask, redirect, url_for, request, render_template
-from pymongo import MongoClient
 import json
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-client = MongoClient(
-    os.environ['DB_PORT_27017_TCP_ADDR'],27017)
-db = client.tododb
+app.config['MONGO_DBNAME'] = 'serverdb'
+app.config['MONGO_URI'] = 'mongodb://mongodb:27017/serverdb'
 
+mongo = PyMongo(app)
+
+@app.route('/users', methods=['GET'])
+def get_all_stars():
+  users = mongo.db.users
+  output = []
+  for user in users.find():
+    output.append({'autor' : user['autor'], 'nota' : user['nota']})
+  return jsonify({'result' : output})
+
+@app.route('/userinsert', methods=['GET'])
+def insert_star():
+  user = mongo.db.users
+  name = "carlos"
+  nota = "hola que tal es mi comentario."
+  user_id = user.insert({'autor': name, 'nota': nota})
+  new_user = user.find_one({'_id': user_id })
+  output = {'autor' : new_user['autor'], 'nota' : new_user['nota']}
+  return jsonify({'result' : output})
+
+@app.route('/user', methods=['POST'])
+def add_star():
+  user = mongo.db.users
+  autor = request.json['autor']
+  nota = request.json['nota']
+  user.insert_one({'autor': autor, 'nota': nota})
+  return jsonify({'result' : 'ok'})
+
+@app.route('/')
+def main_message():
+    return "API PYTHON SOPES"
 
 @app.route('/cpu')
-def todo():
-
-    _items = db.tododb.find()
-    items = [item for item in _items]
-    with open('/app/src/cpu.txt') as json_file:
+def get_cpu():
+    with open('/usr/src/app/cpu.txt') as json_file:
         data = json.load(json_file)
         return data
-    return "hola"
-    #return render_template('todo.html', items=items)
 
-
-@app.route('/new', methods=['POST'])
-def new():
-
-    item_doc = {
-        'name': request.form['name'],
-        'description': request.form['description']
-    }
-    db.tododb.insert_one(item_doc)
-
-    return redirect(url_for('todo'))
+@app.route('/ram')
+def get_cpu():
+    with open('/usr/src/app/ram.txt') as json_file:
+        data = json.load(json_file)
+        return data
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
